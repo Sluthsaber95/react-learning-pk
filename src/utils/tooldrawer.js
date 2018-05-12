01286baedccf61ef0eed5247ddcb9fe81857926b
Anthony Tran <anthonytran.dev@gmail.com>
2018-05-12 11:46:01 +0100

    Created src/utils

---
+++ src/utils/tooldrawer.js
@@ -0,0 +1,9 @@
+const StatelessComponent = () => {
+  const whiteListValues = [<div></div>, "", NaN, 0, null];
+  const index = Math.floor(Math.random()*4)
+  return null;
+}
+
+export default tooldrawer = {
+  StatelessComponent
+}
