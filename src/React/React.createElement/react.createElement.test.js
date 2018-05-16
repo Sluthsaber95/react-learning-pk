d139df3132e5590af72d14e4a9b0ee4b93ef5fa1
Anthony Tran <anthonytran.dev@gmail.com>
2018-05-16 17:11:04 +0100

    Completed Tests React.createElement.test.js

---
+++ src/React/React.createElement/react.createElement.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/cjs/react-dom.development';
+import { shallow, mount, unmount } from 'enzyme'; // 348 K
+import 'console.table'
+import '../../enzyme-setup'
+
+import { } from '../../utils/mockStateless';
+import { } from '../../utils/mockContainers';
+
+
+describe('Testing out React.createElement', () => {
+  it('testing React.createElement() on values returned', () => {
+    try{
+      // expect(React.createElement())
+      const error = true;
+    } catch (error) {
+      // console.error("Warning: React.createElement: type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it'sdefined in, or you might have mixed up default and named imports.")
+    }
+  });
+  it('testing React.createElement(type, [props], [..children]) on values returned', () => {
+    // const type = 'fragment';
+    // const props = null;
+    // const children = `Hello ${this.props.toWhat}`
+    // React.createElement(type, [...props], [...children])
+
+    /* 
+    Not Neccessary if one is not using JSX
+    Suggestions: Do at least 5 sets of examples - to cover all the learning bases
+      1) one with no props, simple string
+      2) one with fragment type, props array and children array - rather than plan strings
+      3) with no JSX at all, good old JavaScript code
+      4) Include propTypes
+    */
+
+    class Hello extends React.Component {
+      render() {
+        return React.createElement('div', null, `Hello ${this.props.toWhat}`);
+      }
+    }
+    const div = document.createElement('div')
+    const wrapper = mount(<Hello toWhat="World"/>)
+
+  });
+})
