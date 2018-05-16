ec4fbb8e7c558c1f0aa5742e6b49ee1a42760486
Anthony Tran <anthonytran.dev@gmail.com>
2018-05-16 13:45:26 +0100

    Reorganised react.component.js and included 2 additional test components

---
+++ src/React/React.Component/react.component.test.js
@@ -0,0 +1,97 @@
+/*
+NOTE: 
+------------------------
+Tests on both Component and PureComponent are the same expect a few difference
+in rendering.
+
+OPTIMISATION:
+------------------------
+- Test messaging service to catch out errors, possibly rather than displaying them with console.log
+- Give each test a unique ID, a hash that changes, when the tests are altered. To make them more searchable
+
+This subtle differences between Component and PureComponent are used as exemplars as so.
+*/
+import React, { Component } from 'react'
+import context from 'jest-plugin-context'
+import { shallow, mount, unmount } from 'enzyme' // 348 K
+import '../../enzyme-setup'
+import 'console.table'
+import {
+  Greeting,
+  Message,
+  PureGreeting,
+  PureMessage
+} from '../../utils/mockContainers'
+
+describe('Testing out React.Component Abstract Class', () => {
+    context('React.Component Minimum Requirements', () => {
+    const messageWrapper = mount(<Message />)
+    const greetingWrapper = mount(<Greeting />)
+
+    it('test the current nodes exists', () => {
+      expect(messageWrapper.exists()).toBeTruthy()
+      expect(greetingWrapper.exists()).toBeTruthy()
+    })
+    it('testing nodes has been rendered', () => {
+      expect(messageWrapper).toHaveLength(1)
+      expect(greetingWrapper).toHaveLength(1)
+      expect(messageWrapper.contains(<h1>Hello, Anthony</h1>)).toBeTruthy()
+    })
+    it('testing the <Message /> has a child component <Greeting />', () => {
+      // either is find second is more specific
+      expect(messageWrapper.find('div').children()).toHaveLength(1)
+      expect(messageWrapper.find(Greeting)).toHaveLength(1)
+    })
+
+    it('test the <Greeting /> can accept props.name', () => {
+      const _greetingWrapper = shallow(<Greeting name="Anthony" />)
+      expect(_greetingWrapper.instance().props.name).toBe("Anthony")
+      _greetingWrapper.setProps({ name: "Samantha" })
+      expect(_greetingWrapper.instance().props.name).toBe("Samantha")
+    })
+    it('test the <Greeting /> can accept props from <Message />', () => {
+      expect(messageWrapper.instance().state.name).toBe("Anthony")
+      expect(messageWrapper.find(Greeting).instance().props.name).toBe("Anthony")
+    })
+  })
+  context('React.PureComponent Minimum Requirements', () => {
+    const pureMessageWrapper = mount(<PureMessage />)
+    const pureGreetingWrapper = mount(<PureGreeting />)
+
+    it('test the current nodes exists', () => {
+      expect(pureMessageWrapper.exists()).toBeTruthy()
+      expect(pureGreetingWrapper.exists()).toBeTruthy()
+    })
+    it('testing nodes has been rendered', () => {
+      expect(pureMessageWrapper).toHaveLength(1)
+      expect(pureGreetingWrapper).toHaveLength(1)
+      expect(pureMessageWrapper.contains(<h1>Hello, Anthony</h1>)).toBeTruthy()
+    })
+    it('testing the <PureMessage /> has a child component <PureGreeting />', () => {
+      // either is find second is more specific
+      expect(pureMessageWrapper.find('div').children()).toHaveLength(1)
+      // console.table({
+      //   'TEST': 'testing the <PureMessage /> has a child component <PureGreeting />',
+      //   'NOTE': 'PureComponents renderes implements it with a shallow props and state comparison - i.e. only goes one layer deep'
+      // })
+      expect(pureMessageWrapper.find(PureGreeting)).not.toHaveLength(1)
+      expect(pureMessageWrapper.find(PureGreeting)).toHaveLength(0)
+    })
+
+    it('test the <PureGreeting /> can accept props.name', () => {
+      const _greetingWrapper = shallow(<PureGreeting name="Anthony" />)
+      expect(_greetingWrapper.instance().props.name).toBe("Anthony")
+      _greetingWrapper.setProps({ name: "Samantha" })
+      expect(_greetingWrapper.instance().props.name).toBe("Samantha")
+    })
+    it('test the <PureGreeting /> can accept props from <PureMessage />', () => {
+      expect(pureMessageWrapper.instance().state.name).toBe("Anthony")
+      try{
+        expect(pureMessageWrapper.find(PureGreeting).instance().props.name).toBe("Anthony")
+      }
+      catch(err){
+        // console.error('ReactWrapper::instance() can only be called on single nodes')
+      }
+    })
+  })
+})
--- src/utils/mockContainers.js
+++ src/utils/mockContainers.js
@@ -1,9 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 export class ContainerEmpty extends Component {
   constructor(props){
     super(props);
   }
   render() {
     return (<div></div>);
+  }
+}
+export class Greeting extends Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>
+  }
+}
+export class Message extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: "Anthony"
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Greeting name={this.state.name} />
+      </div>
+    )
+  }
+}
+export class PureGreeting extends PureComponent {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>
+  }
+}
+export class PureMessage extends PureComponent {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: "Anthony"
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Greeting name={this.state.name} />
+      </div>
+    )
   }
 }
