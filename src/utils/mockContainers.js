50b352f1b092019183d21da3ce54bea03545fca4
Anthony Tran <anthonytran.dev@gmail.com>
2018-05-17 13:45:08 +0100

    Appended Additional Containers

--- src/utils/mockContainers.js
+++ src/utils/mockContainers.js
@@ -47,3 +47,41 @@ export class PureMessage extends PureComponent {
     )
   }
 }
+export class Respondent extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render (){
+    return (
+      <div>
+        { 
+          this.props.children
+          ? React.cloneElement(this.props.children, {...this.props})
+          : <ViewedIndicator {...this.props}/>
+        } 
+      </div>
+    )
+  }
+}
+
+export class ViewedIndicator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      backgroundColor: "Green"
+    }
+  }
+  render(){
+    this.props.respondentSeen 
+      ? this.state.backgroundColor = "Green" 
+      : this.state.backgroundColor = "Red"
+    const circleRadius = '10px'
+    const indicatorStyle = {
+      width: circleRadius,
+      height: circleRadius,
+      borderRadius: '50%',
+      backgroundColor: this.state.backgroundColor
+    }
+    return (<div style={indicatorStyle}></div>)
+  }
+}
