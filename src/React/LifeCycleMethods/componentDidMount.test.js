0d05d79eb568bcf3b6f89dc0d68e43d62235e68c
Anthony Tran <anthonytran.dev@gmail.com>
2018-05-30 00:15:25 +0100

    Created componentDidMount.test.js

---
+++ src/React/LifeCycleMethods/componentDidMount.test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/cjs/react-dom.development';
+import context from 'jest-plugin-context';
+import { shallow, mount, unmount } from 'enzyme'; // 348 K
+import '../../enzyme-setup'
+
+import { } from '../../utils/mockStateless';
+import { } from '../../utils/mockContainers';
+
+describe('Testing out the componentDidMount lifecycle method', () => {
+  context('componentDidMount', () => {
+    it('testing componentDidMount() on values returned', () => {
+    });
+  })
+})
