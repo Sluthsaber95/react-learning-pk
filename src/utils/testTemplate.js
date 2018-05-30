55c6b5fa52f9b53af787efcd897a953b5061962c
Anthony Tran <anthonytran.dev@gmail.com>
2018-05-30 20:47:05 +0100

    Included sinon.js import

--- src/utils/testTemplate.js
+++ src/utils/testTemplate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom/cjs/react-dom.development';
 import context from 'jest-plugin-context';
 import { shallow, mount, unmount } from 'enzyme'; // 348 K
+import sinon from 'sinon';
 import '../../enzyme-setup'
 
 import { } from '../../utils/mockStateless';
